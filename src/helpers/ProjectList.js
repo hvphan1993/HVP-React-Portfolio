49a605e80f35ef34f1d4bc82169fef0bfe5788e4
hvphan <hvphan1993@gmail.com>
2022-08-07 00:07:38 -0700

    adding to projects page, using helpers for project list

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,34 @@
+import budgetTracker from '../assets/budget-tracker.png';
+import crystalJumper from "../assets/crystalJumper.png";
+import whatsMyJam from "../assets/whatsmyjam.png";
+import noteTaker from "../assets/note-taker.png";
+import wethaWizard from "../assets/wethaWizard.png";
+import personalPortfolio from "../assets/personalPortfolio.png";
+
+export const ProjectList = [
+    {
+        name: "Crystal Jumper",
+        image: crystalJumper,
+    },
+    {
+        name:"Balling On A Budget",
+        image:budgetTracker,
+    },
+    {
+        name: "What's My Jam",
+        image: whatsMyJam,
+    },
+    {
+        name: "Note Taker",
+        image: noteTaker,
+    },
+    {
+        name: "Wetha Wizard",
+        image: wethaWizard,
+    },
+    {
+        name: "HTML+CSS Personal Portfolio",
+        image: personalPortfolio,
+    }
+   
+]
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,26 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import budgetTracker from '../assets/budget-tracker.png';
+import crystalJumper from "../assets/crystalJumper.png";
+import whatsMyJam from "../assets/whatsmyjam.png";
+import noteTaker from "../assets/note-taker.png";
+import wethaWizard from "../assets/wethaWizard.png";
+import personalPortfolio from "../assets/personalPortfolio.png";
+import { ProjectList } from "../helpers/ProjectList"; 
+
+import "../styles/Projects.css";
 
 function Projects() {
-    return <div>Projects</div>;
-
+    return (
+        <div className="projects">
+            <h1>My Personal Projects</h1>
+            <div className="projectList">
+            {ProjectList.map((project) => {
+                return <ProjectItem name={project.name} image={project.image} />
+            })}
+            </div>
+        </div>
+    )
 }
 
 export default Projects;
